a6aac6e1e2778ce06b75610dadd720030e905937
王野 <lament@wangyedeMacBook-Pro.local>
2017-08-07 16:54:30 +0800

    change to chinese python2.7

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+# -*- coding:utf-8 -*-
 from flask import Flask, render_template
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 app = Flask(__name__)
 
-english_bot = ChatBot("English Bot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+chinese_bot = ChatBot("Chinese Bot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 
-english_bot.set_trainer(ChatterBotCorpusTrainer)
-english_bot.train("chatterbot.corpus.english")
+chinese_bot.set_trainer(ChatterBotCorpusTrainer)
+chinese_bot.train("chatterbot.corpus.chinese")
 
 
 @app.route("/")
@@ -16,7 +22,7 @@ def home():
 
 @app.route("/get/<string:query>")
 def get_raw_response(query):
-    return str(english_bot.get_response(query))
+    return str(chinese_bot.get_response(query))
 
 
 if __name__ == "__main__":
